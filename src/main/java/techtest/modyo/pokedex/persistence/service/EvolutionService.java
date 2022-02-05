ed3737befb2532426de0565197be914599e7df81
Andrés Osorio <gd_andres98@hotmail.com>
2022-02-04 20:36:04 -0500

    Get next evolution names with dfs

--- src/main/java/techtest/modyo/pokedex/persistence/service/EvolutionService.java
+++ src/main/java/techtest/modyo/pokedex/persistence/service/EvolutionService.java
@@ -2,6 +2,7 @@ package techtest.modyo.pokedex.persistence.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import techtest.modyo.pokedex.persistence.entity.ChainLink;
 import techtest.modyo.pokedex.persistence.entity.EvolutionChain;
 import techtest.modyo.pokedex.persistence.entity.Pokemon;
 import techtest.modyo.pokedex.persistence.entity.PokemonSpecies;
@@ -9,7 +10,10 @@ import techtest.modyo.pokedex.persistence.repository.EvolutionRepository;
 import techtest.modyo.pokedex.persistence.repository.PokemonRepository;
 import techtest.modyo.pokedex.persistence.repository.PokemonSpeciesRepository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.Stack;
 
 @Service
 public class EvolutionService {
@@ -24,17 +28,17 @@ public class EvolutionService {
     private PokemonSpeciesRepository pokemonSpeciesRepository;
 
 
-    public Optional<EvolutionChain> getPokemonEvolutions(int pokemonId) {
+    public Optional<List<String>> getPokemonEvolutions(int pokemonId) {
         Pokemon pokemon = pokemonRepository.getPokemon(pokemonId);
         return getPokemonEvolutions(pokemon);
     }
 
-    public Optional<EvolutionChain> getPokemonEvolutions(String pokemonName) {
+    public Optional<List<String>> getPokemonEvolutions(String pokemonName) {
         Pokemon pokemon = pokemonRepository.getPokemon(pokemonName);
         return getPokemonEvolutions(pokemon);
     }
 
-    private Optional<EvolutionChain> getPokemonEvolutions(Pokemon pokemon) {
+    private Optional<List<String>> getPokemonEvolutions(Pokemon pokemon) {
         if (pokemon == null)
             return Optional.empty();
         PokemonSpecies species = pokemonSpeciesRepository.getPokemonSpecies(pokemon.getSpecies());
@@ -43,8 +47,30 @@ public class EvolutionService {
         int evolutionChainId = species.getEvolutionChainId();
         if (evolutionChainId < 0)
             return Optional.empty();
+        List<String> evolutions = findEvolutionsNames(species.getName(), evolutionRepository.getEvolutionChain(evolutionChainId));
 
-        return Optional.ofNullable(evolutionRepository.getEvolutionChain(evolutionChainId));
+        return Optional.ofNullable(evolutions);
+    }
+
+    private List<String> findEvolutionsNames(String name, EvolutionChain evolutionChain) {
+        if (evolutionChain == null || evolutionChain.getChain() == null)
+            return null;
+        List<String> evolutions = new ArrayList<>();
+        Stack<ChainLink> chainStack = new Stack<>();
+        chainStack.add(evolutionChain.getChain());
+
+        while (!chainStack.empty()) {
+            ChainLink curChain = chainStack.pop();
+            if (curChain.getSpecies().getName().equals(name)) {
+                for (ChainLink childChain : curChain.getEvolvesTo())
+                    evolutions.add(childChain.getSpecies().getName());
+                break;
+            }
+            if (curChain.getEvolvesTo() != null)
+                chainStack.addAll(curChain.getEvolvesTo());
+        }
+
+        return evolutions;
     }
 
 }

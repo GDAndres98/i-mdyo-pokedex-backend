b538e6e17e401f7bf62afc36993a200ed37601a7
Andrés Osorio <gd_andres98@hotmail.com>
2022-02-04 20:36:29 -0500

    PokemonController adjust

--- src/main/java/techtest/modyo/pokedex/web/controller/PokemonController.java
+++ src/main/java/techtest/modyo/pokedex/web/controller/PokemonController.java
@@ -3,14 +3,12 @@ package techtest.modyo.pokedex.web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import techtest.modyo.pokedex.persistence.entity.EvolutionChain;
+import org.springframework.web.bind.annotation.*;
 import techtest.modyo.pokedex.persistence.entity.Pokemon;
 import techtest.modyo.pokedex.persistence.service.EvolutionService;
 import techtest.modyo.pokedex.persistence.service.PokemonService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pokemon")
@@ -22,11 +20,7 @@ public class PokemonController {
     @Autowired
     private EvolutionService evolutionService;
 
-    @RequestMapping("/hello")
-    public String hello() {
-        return "Hello World";
-    }
-
+    @CrossOrigin
     @GetMapping("/{id}")
     public ResponseEntity<Pokemon> getPokemon(@PathVariable("id") int pokemonId) {
         return pokemonService.getPokemon(pokemonId)
@@ -34,6 +28,7 @@ public class PokemonController {
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @CrossOrigin
     @GetMapping("/name/{name}")
     public ResponseEntity<Pokemon> getPokemon(@PathVariable("name") String pokemonName) {
         return pokemonService.getPokemon(pokemonName)
@@ -41,15 +36,17 @@ public class PokemonController {
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @CrossOrigin
     @GetMapping("/{id}/evolutions")
-    public ResponseEntity<EvolutionChain> getPokemonEvolutions(@PathVariable("id") int pokemonId) {
+    public ResponseEntity<List<String>> getPokemonEvolutions(@PathVariable("id") int pokemonId) {
         return evolutionService.getPokemonEvolutions(pokemonId)
                 .map(evolutionChain -> new ResponseEntity<>(evolutionChain, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @CrossOrigin
     @GetMapping("/name/{name}/evolutions")
-    public ResponseEntity<EvolutionChain> getPokemonEvolutions(@PathVariable("name") String pokemonName) {
+    public ResponseEntity<List<String>> getPokemonEvolutions(@PathVariable("name") String pokemonName) {
         return evolutionService.getPokemonEvolutions(pokemonName)
                 .map(evolutionChain -> new ResponseEntity<>(evolutionChain, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
